6428cd979132f60a4919694152922b838b448802
ikramerekabi <ikrame.rekabi@aucegypt.edu>
2024-08-27 13:37:26 +0300

    fixed the code to receive commands and added TTS

--- ROS-master/cloudconnect/streaming_iphone.py
+++ ROS-master/cloudconnect/streaming_iphone.py
@@ -1,31 +1,13 @@
 import sys, os
-from matplotlib import image
-import rospy
-from sensor_msgs.msg import Image
 import cv2
 import numpy as np
 import socketio
 import base64
 import time
-    
-sys.path.append('/usr/local/lib/python2.7/site-packages')
-def image_callback():
-    print("Received an image!")
+import threading
 
-    try:
-    #   im = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
-    #   print(type(im))
-    #   print(im)
-    #   frame = cv2.resize(im,(300,150), interpolation = cv2.INTER_AREA)
-      cap = cv2.VideoCapture(0)
-      while True:
-            # Read a frame from the webcam
-            ret, frame = cap.read()
-            frame = cv2.resize(frame,(300,150), interpolation = cv2.INTER_AREA)
-            streamer.send_data(frame)
-
-    except:
-      print("error")      
+from gtts import gTTS
+from playsound import playsound
 
 class CVClient(object):
     def __init__(self, server_addr, stream_fps):
@@ -36,14 +18,7 @@ class CVClient(object):
         self._wait_t = (1/self._stream_fps)
 
     def _convert_image_to_jpeg(self, image):
-        
-        #frame = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        # Encode frame as jpeg
         frame = cv2.imencode('.jpg', image)[1].tobytes()
-        # Encode frame in base64 representation and remove
-        # utf-8 encoding
-        # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        
         frame = base64.b64encode(frame).decode('utf-8')
         return "data:image/jpeg;base64,{}".format(frame)
 
@@ -52,31 +27,45 @@ class CVClient(object):
         if cur_t - self._last_update_t > self._wait_t:
             self._last_update_t = cur_t
             sio.emit(
-                    'cv2serverphysical',
-                    {
-                        'image': self._convert_image_to_jpeg(frame),
-                    },namespace='/cv')
+                'cv2serverphysical',
+                {
+                    'image': self._convert_image_to_jpeg(frame),
+                }, namespace='/cv'
+            )
 
-    def close(self)
+    def close(self):
         sio.disconnect()
+
+
+def image_callback():
+    print("Received an image!")
+    try:
+        cap = cv2.VideoCapture(0)
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("Failed to capture image")
+                break
+            frame = cv2.resize(frame, (300, 150), interpolation=cv2.INTER_AREA)
+            streamer.send_data(frame)
+    except Exception as e:
+        print("Error in image_callback:", str(e))
+    finally:
+        cap.release()
 
 
 sio = socketio.Client()
 
 
-def convert_image_to_jpeg(image):
-        # Encode frame as jpeg
-        frame = cv2.imencode('.jpg', image)[1].tobytes()
-        # Encode frame in base64 representation and remove
-        # utf-8 encoding
-        frame = base64.b64encode(frame).decode('utf-8')
-        return "data:image/jpeg;base64,{}".format(frame)
-
 @sio.event(namespace='/cv')
 def connect():
     global streamer
-    streamer = CVClient('0.0.0.0', 5.0)
+    streamer = CVClient('localhost', 5.0)
     print('[INFO] Successfully connected to server.')
+    # Start image capturing in a new thread to avoid blocking
+    image_thread = threading.Thread(target=image_callback)
+    image_thread.start()
+    return image_thread
 
 
 @sio.event(namespace='/cv')
@@ -87,8 +76,21 @@ def connect_error():
 @sio.event(namespace='/cv')
 def disconnect():
     print('[INFO] Disconnected from server.')
-	
-	
+
+
+@sio.on('omnideck_data', namespace='/omnideck')
+
+def omnideck_data(data):
+   # print("received "+ str(data))
+    text = "move" + data
+    tts = gTTS(text)
+    tts.save("speech.mp3")
+    playsound("speech.mp3")
+    os.remove("speech.mp3")
+
 if __name__ == '__main__':
-    sio.connect(os.path.expandvars('http://$HOST_IP:8000'))
-    image_callback()
+    # Establish the connection
+    sio.connect('http://localhost:8000', namespaces=['/omnideck', '/cv' ])
+    image_thread = connect()  
+    image_thread.join()
+    streamer.close()
